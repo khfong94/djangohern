3b1a80623392bae254e485ebb43f7602fc0f69cb
Jerome Fong <khfong94@gmail.com>
2016-12-06 21:34:57 +0800

    created dog

--- learning/models.py
+++ learning/models.py
@@ -1,6 +1,13 @@
 from __future__ import unicode_literals
+from django.utils import timezone
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 # Hello
+class Dog(models.Model):
+	color = models.CharField(max_length=15)
+	breed = models.CharField(max_length=20)
+	weight = models.IntegerField()
+	owner = models.ForeignKey(User, default=1)
